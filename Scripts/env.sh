132ce98f2be52ee1ea8da533d2e1f30428145690
Jason Greco <jason.greco@unity3d.com>
2023-05-18 10:36:34 -0700

    update scripts

--- Scripts/env.sh
+++ Scripts/env.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "postbuild script begin"
-env | grep -e 'TEST' -e 'BUILD'
+env | grep -e 'TEST_ENV' -e 'UCB_BUILD'
--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -3,8 +3,9 @@ echo "prebuild script begin"
 
 export TEST_ENV_VAR="abc"
 export TEST_ENV_EXPORT="hello world"
+export UCB_BUILD_NUMBER=123
 
-env | grep -e 'TEST' -e 'BUILD'
+env | grep -e 'TEST_ENV' -e 'UCB_BUILD'
 
 echo "TEST_ENV_EXPORT=$TEST_ENV_EXPORT" >> "$DEVOPS_ENV"
-echo "UCB_BUILD_NUMBER=123" >> "$DEVOPS_ENV"
+echo "UCB_BUILD_NUMBER=$UCB_BUILD_NUMBER" >> "$DEVOPS_ENV"
