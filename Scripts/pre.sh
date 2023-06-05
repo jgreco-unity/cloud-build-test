5dafbbb6876d1588d264aeb0abd077cd1fc0c6a6
Jason Greco <jason.greco@unity3d.com>
2023-06-05 12:46:05 -0700

    Mask env value

--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -6,7 +6,7 @@ export TEST_ENV_EXPORT="hello world"
 export UCB_BUILD_NUMBER=123
 
 export VARIABLE_NAME="HI THERE"
-echo "::mask-value::VARIABLE_NAME"
+echo "::mask-value::$VARIABLE_NAME"
 echo "::mask-value::SOME_VALUE_TO_HIDE"
 echo "There should be stars in place of SOME_VALUE_TO_HIDE and $VARIABLE_NAME"
 
