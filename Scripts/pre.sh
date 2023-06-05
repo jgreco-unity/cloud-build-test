f07189cb3c2b4452b5c412771f5424909fcabfda
Jason Greco <jason.greco@unity3d.com>
2023-06-05 11:42:01 -0700

    Add masked vars

--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -5,7 +5,12 @@ export TEST_ENV_VAR="abc"
 export TEST_ENV_EXPORT="hello world"
 export UCB_BUILD_NUMBER=123
 
-env | grep -e 'TEST_' -e 'UCB_BUILD'
+export VARIABLE_NAME="HI THERE"
+echo "::mask-value::VARIABLE_NAME"
+echo "::mask-value::SOME_VALUE_TO_HIDE"
+echo "There should be stars in place of SOME_VALUE_TO_HIDE and $VARIABLE_NAME"
+
+env | grep -e 'TEST_' -e 'UCB_BUILD' -e 'VARIABLE_NAME'
 
 echo "TEST_ENV_EXPORT=$TEST_ENV_EXPORT" >> "$DEVOPS_ENV"
 echo "UCB_BUILD_NUMBER=$UCB_BUILD_NUMBER" >> "$DEVOPS_ENV"
