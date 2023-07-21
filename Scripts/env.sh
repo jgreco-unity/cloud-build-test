f7368e7d74469e057e1202e97446e829061b758e
Jason Greco <jason.greco@unity3d.com>
2023-07-21 09:54:11 -0700

    Update scripts

--- Scripts/env.sh
+++ Scripts/env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "postbuild script begin"
 env | grep -e 'TEST_' -e 'UCB_BUILD'
-find . -name ".gradle"
-find . -name ".gradle" | xargs ls -l
+find ../../.. -name ".gradle"
+find ../../.. -name ".gradle" | xargs ls -l
