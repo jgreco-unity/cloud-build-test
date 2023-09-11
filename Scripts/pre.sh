b0ac8319b5b1cc11c478707f57e0b885b95cd1a7
Jason Greco <jason.greco@unity3d.com>
2023-09-11 12:48:02 -0700

    Update scripts

--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -16,7 +16,7 @@ echo "::mask-value::$VARIABLE_NAME"
 echo "::mask-value::SOME_VALUE_TO_HIDE"
 echo "There should be stars in place of SOME_VALUE_TO_HIDE and $VARIABLE_NAME"
 
-env | grep -e 'TEST_' -e 'UCB_BUILD' -e 'VARIABLE_NAME'
+env
 
 echo "TEST_ENV_EXPORT=$TEST_ENV_EXPORT" >> "$DEVOPS_ENV"
 echo "UCB_BUILD_NUMBER=$UCB_BUILD_NUMBER" >> "$DEVOPS_ENV"
