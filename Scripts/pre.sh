812e7845fb086d66488b3318dad5e0677bd63a95
jason.greco <jason.greco@unity3d.com>
2023-11-06 16:23:48 -0800

    update script

--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -1,22 +1,16 @@
-#!/bin/sh
+##!/usr/bin/env bash
+
 echo "prebuild script begin"
 
-ls /
+## Source profile
+. ~/.profile
 
-find /Applications/Unity.app/ -type d -name AndroidPlayer
+pwd
 
-pwd | rev
-
-export TEST_ENV_VAR="abc"
-export TEST_ENV_EXPORT="hello world"
-export UCB_BUILD_NUMBER=123
-
-export VARIABLE_NAME="HI THERE"
-echo "::mask-value::$VARIABLE_NAME"
-echo "::mask-value::SOME_VALUE_TO_HIDE"
-echo "There should be stars in place of SOME_VALUE_TO_HIDE and $VARIABLE_NAME"
-
-env
-
-echo "TEST_ENV_EXPORT=$TEST_ENV_EXPORT" >> "$DEVOPS_ENV"
-echo "UCB_BUILD_NUMBER=$UCB_BUILD_NUMBER" >> "$DEVOPS_ENV"
+SCRIPTS=/cygdrive/c/Users/buildbot/*.sh
+ls $SCRIPTS
+for f in $SCRIPTS; do
+    echo "Printing $f"
+    cat "$f"
+    echo "-----"
+done
