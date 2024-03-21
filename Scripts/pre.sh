a21832fe68e328a76dd26f6a1d9af726cfa396a8
jason.greco <jason.greco@unity3d.com>
2024-03-21 11:52:07 -0700

    update scripts

--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 echo "prebuild script begin"
-
+echo "line" >> Readme.md
 env
-
-curl -sLo ugs_installer ugscli.unity.com/v1 && shasum -c <<<"3bbc507d4776a20d5feb4958be2ab7d4edcea8eb  ugs_installer" && version=1.0.0 bash ugs_installer
-ugs -h
-
