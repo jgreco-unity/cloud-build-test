c88b8321c8441d9cf605e9961b85f210ccc208e6
jason.greco <jason.greco@unity3d.com>
2024-04-25 17:59:17 -0700

    update scripts

--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -2,6 +2,6 @@
 echo "prebuild script begin"
 ssh -V
 ls -l ~/.ssh/config
-echo '    HostKeyAlgorithms +ssh-rsa' >> ~/.ssh/config
+echo '\n    HostKeyAlgorithms +ssh-rsa' >> ~/.ssh/config
 cat ~/.ssh/config
 echo "prebuild script end"
