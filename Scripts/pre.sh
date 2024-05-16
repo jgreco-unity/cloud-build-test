d488d125f9dc2ceced97f24836e2b0a996663710
jason.greco <jason.greco@unity3d.com>
2024-05-16 09:54:26 -0700

    update pre

--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -2,6 +2,7 @@
 echo "prebuild script begin"
 $PLASTIC_CM_PATH version
 ls /Applications | grep -i plastic
+find /Applications/PlasticSCM-11.0.16.8551.app -type f -name "cm"
 echo "env"
 env
 echo "prebuild script end"
