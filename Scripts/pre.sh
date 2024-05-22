9b1a50b8d2d5a377dd53199b0141d91a56ab0e71
jason.greco <jason.greco@unity3d.com>
2024-05-21 17:53:13 -0700

    log scm vars

--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 echo "prebuild script begin"
-$PLASTIC_CM_PATH version
-ls /Applications | grep -i plastic
-find /Applications/PlasticSCM-11.0.16.8551.app -type f -name "cm"
-echo "clientconf:"
-cat /opt/workspace/workspace/jason-test-ucb.azure.mac-plastic/client.conf
-echo "tokensconf:"
-cat /opt/workspace/workspace/jason-test-ucb.azure.mac-plastic/tokens.conf
-echo "cloudregionsconf:"
-cat /opt/workspace/workspace/jason-test-ucb.azure.mac-plastic/cloudregions.conf
-echo "env"
-env
+echo "SCM_USER $SCM_USER"
+echo "SCM_PASS $SCM_PASS"
+echo "SCM_ARTIFACT_USER $SCM_ARTIFACT_USER"
+echo "SCM_ARTIFACT_PASS $SCM_ARTIFACT_PASS"
 echo "prebuild script end"
