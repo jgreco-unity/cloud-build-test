7e259907e35c3f3d5cdd57ba0916ff0ca785c990
jason.greco <jason.greco@unity3d.com>
2024-05-21 19:18:36 -0700

    log scm vars

--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -2,6 +2,7 @@
 echo "prebuild script begin"
 echo "SCM_USER $SCM_USER"
 echo "SCM_PASS $SCM_PASS"
+echo "SCM_OAUTH_TOKEN $SCM_OAUTH_TOKEN"
 echo "SCM_ARTIFACT_USER $SCM_ARTIFACT_USER"
 echo "SCM_ARTIFACT_PASS $SCM_ARTIFACT_PASS"
 echo "prebuild script end"
