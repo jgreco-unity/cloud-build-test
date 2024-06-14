fac836d70fbd0ecf68743aa3941e1e5449b57db0
jason.greco <jason.greco@unity3d.com>
2024-06-13 18:02:28 -0700

    update scripts

--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 echo "prebuild script begin"
-$PLASTIC_CM_PATH version
-ls /Applications | grep -i plastic
-find /Applications/PlasticSCM-11.0.16.8551.app -type f -name "cm"
-echo "clientconf:"
-cat /opt/workspace/workspace/jason-test-ucb.azure.mac-plastic/client.conf
-echo "tokensconf:"
-cat /opt/workspace/workspace/jason-test-ucb.azure.mac-plastic/tokens.conf
-echo "cloudregionsconf:"
-cat /opt/workspace/workspace/jason-test-ucb.azure.mac-plastic/cloudregions.conf
+ugs --version
+ls /opt/Unity/windows/scm/plastic/
+/opt/Unity/windows/scm/plastic/PlasticSCM-11.0.16.8622/client/cm.exe version
 echo "env"
 env
 echo "prebuild script end"
