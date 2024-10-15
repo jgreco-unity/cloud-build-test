23dff7c407bd22cde5ff100a9254a598ecf60cf7
jason.greco <jason.greco@unity3d.com>
2024-10-15 16:31:55 -0700

    update scripts

--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 echo "prebuild script begin"
-ugs --version
-ls /opt/Unity/windows/scm/plastic/
-/opt/Unity/windows/scm/plastic/PlasticSCM-11.0.16.8622/client/cm.exe version
 echo "env"
 env
 echo "prebuild script end"
