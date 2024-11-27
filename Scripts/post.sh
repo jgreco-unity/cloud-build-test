8dbed80469f15da4f617c9407cd43b8464236b59
jason.greco <jason.greco@unity3d.com>
2024-11-27 09:12:07 -0800

    update scripts

--- Scripts/post.sh
+++ Scripts/post.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "postbuild script begin"
 pwd
-echo $PROJECT_DIRECTORY
+echo $OUTPUT_DIRECTORY
 find . -name 'edit_mode_tests.xml'
 find . -name 'play_mode_tests.xml'
-edit_mode_test_log=$(<"${PROJECT_DIRECTORY}/Editor/Resources/test_data/edit_mode_tests.xml")
+edit_mode_test_log=$(<"${OUTPUT_DIRECTORY}/Editor/Resources/test_data/edit_mode_tests.xml")
 echo "postbuild script end"
