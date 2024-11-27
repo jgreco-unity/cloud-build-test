d830516aa271cb04981335c398ad58b6d149dd9d
jason.greco <jason.greco@unity3d.com>
2024-11-26 21:49:10 -0800

    update scripts

--- Scripts/post.sh
+++ Scripts/post.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "postbuild script begin"
 pwd
-find .. -name 'edit_mode_tests.xml'
-find .. -name 'play_mode_tests.xml'
+find . -name 'edit_mode_tests.xml'
+find . -name 'play_mode_tests.xml'
 echo "postbuild script end"
