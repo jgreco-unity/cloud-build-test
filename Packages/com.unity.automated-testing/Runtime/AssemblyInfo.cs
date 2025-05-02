e0c9250c9f808675b59c4cee818bde04b82fd8fe
jason.greco <jason.greco@unity3d.com>
2025-05-02 12:10:27 -0700

    new project

--- Packages/com.unity.automated-testing/Runtime/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.AutomatedQA.Tests")]
-[assembly: InternalsVisibleTo("Unity.AutomatedQA.EditorTests")]
--- Scripts/postBuild.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-#echo "CloudBuildPostBuild: $AUTH_TOKEN"
