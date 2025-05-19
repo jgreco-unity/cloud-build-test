00a4e43398314371653560af8834b6ed1ef2b190
jason.greco <jason.greco@unity3d.com>
2025-05-19 14:02:38 -0700

    update scripts

--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 echo "prebuild script begin"
-echo "env"
-env
+curl --location 'https://build-api-stg.cloud.unity3d.com/api/v1/orgs/20066339085561/projects/d6a69f98-ad69-4351-84c5-732759c78803/cloud-build/setup/buildTarget/env/builds/67/builder/status/postbuild' \
+--header 'Content-Type: application/json' \
+--header "Authorization: $API_KEY" \
+--data '{
+    "finishTime": "2025-05-19T23:56:07"
+}'
 echo "prebuild script end"
